d6cd6f44d444244f423a197a581c329445cde80b
EJ Hammond <ehammond@vistaprint.com>
2018-12-23 11:06:08 -0500

    Remove unused import

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import { reducer, initialState } from './rootReducer';
+import { reducer } from './rootReducer';
 import { BudgeState } from './rootState';
 
 const enhancers = composeWithDevTools(applyMiddleware(thunk));
